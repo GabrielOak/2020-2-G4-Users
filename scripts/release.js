8b1c82c3b39af90fbc1f9685da28a2d7e25e23e6
Gabriela Guedes <gabrielabguedes@gmail.com>
2021-05-05 23:30:29 -0300

    Start scripts to commit
    Signed-off-by: Gabriela Guedes <gabrielabguedes@gmail.com>

--- scripts/release.js
+++ scripts/release.js
@@ -1,10 +1,15 @@
-const { Octokit } = require("@octokit/core")
+let { Octokit } = require("@octokit/core")
 const ghReleaseAssets = require('gh-release-assets')
 const axios = require('axios')
 const fs = require('fs')
+const moment = require('moment')
+
 require('dotenv').config()
 
-const { SONAR_URL } = require('./consts.js')
+// Octokit = Octokit.plugin(require('octokit-commit-multiple-files'));
+
+const { SONAR_URL, REPO, OWNER,   SAVE_SONAR_REPO,
+  SAVE_SONAR_OWNER } = require('./consts.js')
 
 const { TOKEN, RELEASE_MINOR } = process.env;
 
@@ -12,8 +17,8 @@ const octokit = new Octokit({ auth: TOKEN});
 
 const getLatestRelease = async () => {
   const releases = await octokit.request('GET /repos/{owner}/{repo}/releases', {
-    owner: 'GabrielOak',
-    repo: '2020-2-G4-Users'
+    owner: OWNER,
+    repo: REPO
   })
   return releases.data[0].tag_name  
 }
@@ -30,42 +35,77 @@ const newTagName = async () => {
   }
 }
 
-const createRelease = async () => {
-  const tag = await newTagName()
+const createRelease = async (tag) => {
   const res = await octokit.request('POST /repos/{owner}/{repo}/releases', {
-    owner: 'GabrielOak',
-    repo: '2020-2-G4-Users',
+    owner: OWNER,
+    repo: REPO,
     tag_name: tag,
     name: tag
   })
   return res.data.upload_url
 }
 
-const saveSonarFile = async () => {
+const generateFilename = (tag) => {
+  let date = moment().format('YYYY-MM-DD');
+  return tag + '_' + date + '.json'
+}
+
+const saveSonarFile = async (filename) => {
   await axios.get(SONAR_URL)
     .then((res) => {
-      fs.writeFileSync('/tmp/sonar.json', JSON.stringify(res.data))
+      fs.writeFileSync(`/tmp/${filename}`, JSON.stringify(res.data))
     })
 }
 
-const uploadSonarFile = async (upload_url) => {
-  await saveSonarFile()
+const uploadSonarFileToRelease = async (upload_url, tag) => {
+  const filename = generateFilename(tag)
+  await saveSonarFile(filename)
   ghReleaseAssets({
     url: upload_url,
     token: [TOKEN],
     assets: [
-      '/tmp/sonar.json',
+      `/tmp/${filename}`,
       {
-        name: 'sonar.json',
-        path: '/tmp/sonar.json'
+        name: filename,
+        path: `/tmp/${filename}`
       }
     ]
   })
 }
 
+const commitSonarFile = async (tag, filename) => {
+  const repoSplit = REPO.split('-')
+  const folder = repoSplit[repoSplit.length - 1]
+
+  await octokit.repos.createForAuthenticatedUser({
+    name: SAVE_SONAR_REPO,
+    description: "testing uploading an image through the GitHub API",
+  })
+
+  const commit = await octokit.repos.createOrUpdateFiles({
+    SAVE_SONAR_OWNER,
+    SAVE_SONAR_REPO,
+    // 'main',
+    // false,
+    changes: [
+      {
+        message: `Add sonar file of ${tag} ${REPO}`,
+        files: {
+          "bla.md": "# vai \n"
+        },
+      }
+    ],
+  });
+}
+
 const script = async () => {
-  const release = await createRelease()
-  await uploadSonarFile(release)
+  const tag = await newTagName()
+  const filename = generateFilename(tag)
+  await saveSonarFile(filename)
+  await commitSonarFile(tag, filename)
+
+  // const release = await createRelease(tag)
+  // await uploadSonarFileToRelease(release, tag)
 }
 
 script()
